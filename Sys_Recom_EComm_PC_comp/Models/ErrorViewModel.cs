99b8a77644a0adcf47342deb4f1c91246fb222af
StanciuAlin <stanciualin26@gmail.com>
2020-10-28 22:29:02 +0200

    Add frontend structure using AdminLTE

---
+++ Sys_Recom_EComm_PC_comp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sys_Recom_EComm_PC_comp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
